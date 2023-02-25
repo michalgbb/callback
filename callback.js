76c6eccec7b32c346f06a23ae256591207157481
michalgbb <michalgbb@op.pl>
2023-02-25 16:28:59 +0100

    Initalized

---
+++ callback.js
@@ -0,0 +1,20 @@
+const arr = [1, 3, 5]
+
+const multiplay = (param) => param * 2;
+const add = param => param + 20;
+const odd = param => param - 1;
+
+const zmienTablice = (pierwsza, callback) => {
+    for(let i=0; i < pierwsza.length; i++) {
+        pierwsza[i] = callback(pierwsza[i]);
+    }
+}
+
+zmienTablice(arr, dodawanie);
+console.log(arr);
+
+//callback mozemy przesyłac jako funkcje nazwana lub jako funkcje anonimowa
+
+//callback to wymyślne słowo na funkcję przekazaną jako parametr
+
+//bardzo czesto callback stosujemy jako asynchronicznosc aby cos się przed czymś wykonało
